83703d29471a6f94a69d607e99d517a66d8ae596
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-20 15:28:32 +0200

    Add: Lumberjack class Add: Flapjack enum

---
+++ EnumsAndCollectionsExercise06/Lumberjack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndCollectionsExercise06
+{
+    class Lumberjack
+    {
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+        public void EatFlapjacks()
+        {
+            foreach (Flapjack flapjack in flapjackStack)
+            {
+                Console.WriteLine($"{Name} ate a {flapjack} flapjack\n{flapjackStack.Pop()}");
+            }
+        }
+    }
+}
