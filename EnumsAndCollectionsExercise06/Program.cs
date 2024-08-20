b44436a7cd013fd477a859c8d6b3cf00057257cd
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-20 18:51:38 +0200

    Add: rest of main method

--- EnumsAndCollectionsExercise06/Program.cs
+++ EnumsAndCollectionsExercise06/Program.cs
@@ -11,13 +11,30 @@ namespace EnumsAndCollectionsExercise06
         static Random random = new Random();
         static void Main(string[] args)
         {
-            Console.Write("First lumberjack's name: ");
-            Lumberjack lumberjack = new Lumberjack(Console.ReadLine());
-            Console.Write("Number of flapjacks: ");
-            if (int.TryParse(Console.ReadLine(), out int flapcjackNumber))
-                for (int i = 0; i < flapcjackNumber; i++)
-                    lumberjack.TakeFlapjack((Flapjack)random.Next(4));
-            lumberjack.EatFlapjacks();
+            Queue<Lumberjack> lumberjacks = new Queue<Lumberjack>();
+            while (true)
+            {
+                if (lumberjacks.Count == 0)
+                    Console.Write("First lumberjack's name: ");
+                else
+                    Console.Write("Next lumberjack's name (blank to end): ");
+                string line = Console.ReadLine();
+                if (line != "")
+                {
+                    Lumberjack lumberjack = new Lumberjack(line);
+                    Console.Write("Number of flapjacks: ");
+                    if (int.TryParse(Console.ReadLine(), out int flapcjackNumber))
+                        for (int i = 0; i < flapcjackNumber; i++)
+                            lumberjack.TakeFlapjack((Flapjack)random.Next(4));
+                    lumberjacks.Enqueue(lumberjack);
+                }
+                else
+                    break;
+            }
+            while (lumberjacks.Count > 0)
+            {
+                lumberjacks.Dequeue().EatFlapjacks();
+            }
         }
     }
 }
